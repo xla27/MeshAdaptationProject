a09369046bc975f3b2e93c80d94a44d8b283c948
xla27 <alberto.perlini99@gmail.com>
2025-07-17 11:53:26 +0200

    Code Architecture

---
+++ CDriver.py
@@ -0,0 +1,19 @@
+# Struttura codice
+
+# lettura mesh .su2
+# 1) creazione di Cvertex per ogni vertex contente coordinate, punti neighbours e elementi neighbours 
+# 2) creazione di Celement per ogni elemento contenente ID e coordinate vertici, ID elementi patch
+# 3) conversione in file .mesh
+
+# lettura soluzione dal restart .csv
+# 1) loading ad ogni CVertex di valore sensore e valore gradiente sensore (GG)
+
+# main loop sugli elementi 
+# 1) interpolazione dai vertici alla cella
+# 2) calcolo della G
+# 3) calcolo e autodecomposizione della jacobiana
+# 4) nuovi autovalori e autovettori
+# 5) calcolo metrica
+
+# loop sui vertices per i calcolo della media nodewise
+# scrittura file .sol con metrica nodewise
---
+++ CVertex.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class CVertex():
+
+    def __init__(self):
+        return
+    
+    def SetID(self, ID):
+        self.ID = ID
+    
+    def SetCoordinates(self, x, y):
+        self.x = x
+        self.y = y
+
+    def SetSolution(self, solution):
+        self.solution = solution
+
+    def SetGradient(self, gradient):
+        self.gradient = gradient
+
+    def SetVerticesNeighboursID(self, vertex_IDs):
+        self.vertices_neighbours_IDs = vertex_IDs
+
+    def SetElementsNeighboursID(self, elements_IDs):
+        self.elements_neighbours_IDs = elements_IDs
+
+    def GetID(self):
+        return self.ID
+    
+    def GetCoordinates(self):
+        return self.x, self.y
+    
+    def GetSolution(self):
+        return self.solution
+    
+    def GetGradient(self):
+        return self.gradient
+    
+    def GetVerticesNeighboursID(self):
+        return self.vertices_neighbours_IDs 
+
+    def GetElementsNeighboursID(self):
+        return self.elements_neighbours_IDs 
+
+
+
+
