1952262bbb5094a075cfe1c9e0510a6b9aa5fb34
xla27 <alberto.perlini99@gmail.com>
2025-07-18 09:44:30 +0200

    Update CDriver

--- CDriver.py
+++ CDriver.py
@@ -28,11 +28,64 @@ class CDriver():
     def __init__(self, parameters):
         return
     
-    def ReadSU2(self, meshFilename, solFilename):
+    def Initialize(self, meshFilename, solFilename):
 
-        mesh = CMesh()
+        self.mesh = CMesh()
 
         # reading mesh .su2
-        mesh.ReadMeshSU2(meshFilename)
+        self.mesh.ReadMeshSU2(meshFilename)
 
-        #
+        # reading the solution
+        self.mesh.ReadSolSU2(solFilename)
+
+        # finalizing data structure
+        self.mesh.FinalizingDataStructure()
+
+    def Run(self):
+
+        # mesh data structure
+        meshDict = self.mesh.GetMeshDict()
+
+        dim = self.mesh.GetDim()
+
+        keyElem = 'Triangles' if dim == 2 else 'Tetrahedra'
+
+        # computing the element-wise metric
+        for element in meshDict[keyElem]:
+
+            # compute element area
+            element.ComputeArea()
+
+            # computing the gradient on the element
+            element.ComputeGradient()
+
+            # creating the element patch
+            element.SetPatchID()
+            element.SetPatchElements(self.mesh)
+
+            # computing the patch area
+            element.ComputePatchArea()
+
+            # computing the average patch gradient
+            element.ComputePatchAveragedGradient()
+
+            element.ComputeMetric()
+
+        # computing the vertex-wise metric
+        for vertex in meshDict['Vertices']:
+            
+            vertex.ComputeMetric(self.mesh)
+
+    def Finalize(self, meditFilename, solFilename):
+
+        # writing the mesh in medit format
+        self.mesh.WriteMeshMedit(meditFilename)
+
+        # writing the solution in sol format
+        self.mesh.WriteSolMedit(solFilename)
+
+
+
+    
+
+
