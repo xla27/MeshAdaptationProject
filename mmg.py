50478dc281ce5f96125152a6a0f07b4aa472a09e
xla27 <alberto.perlini99@gmail.com>
2025-09-03 16:18:47 +0200

    Fixing results printing

--- mmg.py
+++ mmg.py
@@ -244,7 +244,9 @@ def mmg(config):
             #--- Writing the MMG mesh, sol and param file
             driver.WriteMedit(meshfil.replace('.su2', '.mesh'),
                               solfil.replace(sol_ext_cfd, '.sol'))
-
+            
+            #--- Print mesh sizes
+            print_adap_table(iSub, driver.mesh.meshDict, globalAnisoError, limitedElements, elapsedTime)
 
             #--- Adapt mesh with MMG
             meshin = config_cfd['MESH_FILENAME'].replace('.su2','.mesh')
@@ -258,9 +260,6 @@ def mmg(config):
 
             #--- Writing the adapted mesh in SU2 format
             driver.WriteSU2(meshout.replace('.mesh','.su2'))
-
-            #--- Print mesh sizes
-            print_adap_table(iSub, driver.mesh.meshDict, globalAnisoError, limitedElements, elapsedTime)
 
             dir = f'./ite{global_iter}'
             os.makedirs(os.path.join('..',dir))
@@ -295,9 +294,10 @@ def mmg(config):
             except:
                 raise
 
-            del driver
-
     #--- Write final files
+    driver.ReadSU2()
+    globalAnisoError, limitedElements, elapsedTime =  driver.ComputeMetricAndAnisoError()
+    print_adap_table(iSub+1, driver.mesh.meshDict, globalAnisoError, limitedElements, elapsedTime)
 
     # fileconverter = MeshSolConverter()
     # fileconverter.SU2ToMeditMesh(meshfil, meshfil.replace('.su2', '.mesh'))
